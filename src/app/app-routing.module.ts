d1984b23b369396b74192cbd169dc14646a0ae2b
frankieavina <frankieavina@gmail.com>
2021-08-24 01:25:45 -0700

    finished alignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 ];
 
+// fragment Router Link scrolling
+// html: routerLink="." fragment="section-a" other html= id="section-a"
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      anchorScrolling: 'enabled',
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
