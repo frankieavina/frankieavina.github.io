f868cbc7d8c0a1385fe60b22ce2fc54077d65644
frankieavina <frankieavina@gmail.com>
2021-08-25 01:16:35 -0700

    Added Resume PDF Link

--- src/app/navbar/header/header.component.ts
+++ src/app/navbar/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var require: any
+const FileSaver = require('file-saver');
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  openResume(){
+    const pdfUrl = './assets/web-developer-francisco-avina.pdf';
+    window.open(pdfUrl, '_blank','',true);
+  }
+
+  // downloadPdf() {
+  //   const pdfUrl = './assets/web-developer-francisco-avina.pdf';
+  //   const pdfName = 'Frankies Resume';
+  //   FileSaver.saveAs(pdfUrl, pdfName);
+  // }
+
 
 }
